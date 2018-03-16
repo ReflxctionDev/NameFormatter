d23ffc3f71ec30dfc4c92af7e48bc206f448561f
BuildTools <unconfigured@null.spigotmc.org>
2018-03-16 20:28:24 +0200

    Updated mod info

--- src/main/java/net/reflxction/nameformatter/Reference.java
+++ src/main/java/net/reflxction/nameformatter/Reference.java
@@ -3,7 +3,7 @@ package net.reflxction.nameformatter;
 public class Reference {
 
     public static final String PREFIX = ChatColor.translateAlternateColorCodes("&e[&dName Formatter&e] ");
-    static final String MOD_ID = "nameformatter";
+    static final String MOD_ID = "name-formatter";
     static final String NAME = "NameFormatter";
     static final String VERSION = "1.2";
     static final String ACCEPTED_VERSIONS = "[1.8.9]";
